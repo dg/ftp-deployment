c0045baa83357ae4368cfc0f71474d7c2ebdcfc6
David Grudl <david@grudl.com>
2014-09-02 11:22:15 +0200

    simplified phpDoc

--- Deployment/libs/Logger.php
+++ Deployment/libs/Logger.php
@@ -4,9 +4,6 @@
  * FTP Deployment
  *
  * Copyright (c) 2009 David Grudl (http://davidgrudl.com)
- *
- * For the full copyright and license information, please view
- * the file license.txt that was distributed with this source code.
  */
 
 
