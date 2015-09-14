c0a06ceb7e42ae04d7037a86b4dd5073791e52ea
David Grudl <david@grudl.com>
2015-09-14 15:54:55 +0200

    deployment.php: added PHP version check

--- src/deployment.php
+++ src/deployment.php
@@ -2,6 +2,11 @@
 
 namespace Deployment;
 
+if (PHP_VERSION_ID < 50400) {
+	echo 'Error: Deployment requires PHP 5.4.0 or newer.';
+	exit(1);
+}
+
 require __DIR__ . '/Deployment/Server.php';
 require __DIR__ . '/Deployment/FtpServer.php';
 require __DIR__ . '/Deployment/SshServer.php';
