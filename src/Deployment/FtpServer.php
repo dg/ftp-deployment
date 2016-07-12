ab6f6c2c0e334ac929373133e4bb31b6b6779ed0
David Grudl <david@grudl.com>
2016-07-12 14:02:36 +0200

    FtpServer: throws exception when ftps is not supported

--- src/Deployment/FtpServer.php
+++ src/Deployment/FtpServer.php
@@ -41,6 +41,8 @@ class FtpServer implements Server
 		$this->url = $url = is_array($url) ? $url : parse_url($url);
 		if (!isset($url['scheme'], $url['user'], $url['pass']) || ($url['scheme'] !== 'ftp' && $url['scheme'] !== 'ftps')) {
 			throw new \InvalidArgumentException("Invalid URL or missing username or password");
+		} elseif ($url['scheme'] === 'ftps' && !function_exists('ftp_ssl_connect')) {
+			throw new \Exception('PHP extension OpenSSL is not built statically in PHP.');
 		}
 		$this->passiveMode = (bool) $passiveMode;
 	}
